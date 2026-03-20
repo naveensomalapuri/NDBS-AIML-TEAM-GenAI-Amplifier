51298d4eff489c10e899751d759cbe6e61f208dd
Naveen Somalapuri <somalapurinaveen@gmail.com>
2026-03-20 22:35:26 +0530

    Update main logic

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-# Mount static files
+# Mount static files..
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Set up templates
